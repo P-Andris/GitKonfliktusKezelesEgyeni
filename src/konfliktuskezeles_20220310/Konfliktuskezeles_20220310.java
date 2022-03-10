4c6b05a0b08a0e6caca2e81ffa4857848f457023
P-Andris <pillerandras0714@gmail.com>
2022-03-10 12:29:07 +0100

    Első név inicializálása

--- src/konfliktuskezeles_20220310/Konfliktuskezeles_20220310.java
+++ src/konfliktuskezeles_20220310/Konfliktuskezeles_20220310.java
@@ -3,7 +3,7 @@ package konfliktuskezeles_20220310;
 public class Konfliktuskezeles_20220310 {
 
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "Piller András";
         String nev2;
     }
     
