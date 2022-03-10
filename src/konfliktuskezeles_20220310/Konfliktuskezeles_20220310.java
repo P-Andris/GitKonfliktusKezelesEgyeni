a9e6dfb6acd42d906e4559e8949916bde1732e8a
P-Andris <pillerandras0714@gmail.com>
2022-03-10 12:40:34 +0100

    Harmadik név deklarálása

--- src/konfliktuskezeles_20220310/Konfliktuskezeles_20220310.java
+++ src/konfliktuskezeles_20220310/Konfliktuskezeles_20220310.java
@@ -3,9 +3,10 @@ package konfliktuskezeles_20220310;
 public class Konfliktuskezeles_20220310 {
 
     public static void main(String[] args) {
+        int i = 7;
         String nev1 = "Piller András";
         String nev2 = "x.y.";
-        
+        int nev3;
     }
     
 }
